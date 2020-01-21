6a142b9a6c01f39a458cf0bf90fa24f615ef6c82
pratiti <pratitipshah@gmail.com>
2020-01-21 19:52:56 +0530

    Started : Address book using oops concepts

---
+++ AddressBook/Person.js
@@ -0,0 +1,21 @@
+class Person{
+    constructor(id,firstName,lastName,address,city,state,zip){
+        this.id=id;
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.address=address;
+        this.city=city;
+        this.state=state;
+        this.zip=zip;
+    }
+
+    getPerson(id){
+
+    }
+
+    editPerson(id){
+        
+    }
+
+
+}
